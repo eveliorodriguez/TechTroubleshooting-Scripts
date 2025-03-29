80252d9439bb5a884c43c8d258cfe030874b72ba
Evelio Rodriguez <eveliorodriguezinfo@gmail.com>
2025-03-29 13:04:07 -0400

    Added script to check ip configurations

---
+++ check_ipconfig.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Network Configuration
+echo ----------------------------------------------
+ipconfig /all
+echo ----------------------------------------------
+echo - IPv4: Shows assigned IP Address
+echo - Default Gateway: Your router's IP
+echo - DNS Servers: Checks for misconfigured DNS
+pause
